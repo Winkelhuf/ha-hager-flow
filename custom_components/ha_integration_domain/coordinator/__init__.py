828cc6520493ffa3629e5e54eb6b471db427b400
Winkelhuf <154673811+Winkelhuf@users.noreply.github.com>
2026-09-19 17:18:54 +0200

    Modify coordinator to import HagerFlowCoordinator
    Updated the coordinator module to include HagerFlowCoordinator.

--- custom_components/ha_integration_domain/coordinator/__init__.py
+++ custom_components/ha_integration_domain/coordinator/__init__.py
@@ -1,5 +1,2 @@
-"""Data update coordinator package for ha_integration_domain."""
-
-from .base import IntegrationBlueprintDataUpdateCoordinator
-
-__all__ = ["IntegrationBlueprintDataUpdateCoordinator"]
+"""Coordinator-Modul."""
+from .base import HagerFlowCoordinator
